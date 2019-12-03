38e8e76f2253c267693fcf9543f2e0bfb47cc078
bky373 <bo373@naver.com>
2019-12-03 13:27:05 +0900

    add function of textToSpeech

--- app/src/main/java/com/example/rockpaperscissorsgame/MainActivity.java
+++ app/src/main/java/com/example/rockpaperscissorsgame/MainActivity.java
@@ -5,14 +5,20 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.graphics.drawable.AnimationDrawable;
 import android.media.Image;
 import android.os.Bundle;
+import android.speech.tts.TextToSpeech;
 import android.view.View;
 import android.widget.ImageView;
+
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
     ImageView handAnimImageView;
     ImageView setHandImageView;
+
     AnimationDrawable animationDrawable;
+
+    TextToSpeech textToSpeech;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +31,17 @@ public class MainActivity extends AppCompatActivity {
 
         animationDrawable = (AnimationDrawable) handAnimImageView.getDrawable();
 
+        textToSpeech = new TextToSpeech(getApplicationContext(), new TextToSpeech.OnInitListener() {
+            @Override
+            public void onInit(int status) {
+                if (status != TextToSpeech.ERROR) {
+                    textToSpeech.setLanguage(Locale.KOREAN);
+                    textToSpeech.setPitch(1.0f); // 음성의 높낮이
+                    textToSpeech.setSpeechRate(1.0f); // 음성의 속도
+                }
+            }
+        });
+
     }
 
     public void button_click(View view) {
@@ -33,6 +50,7 @@ public class MainActivity extends AppCompatActivity {
                 setHandImageView.setVisibility(View.GONE);
                 handAnimImageView.setVisibility(view.VISIBLE);
                 animationDrawable.start();
+                textToSpeech.speak("가위바위보", TextToSpeech.QUEUE_FLUSH, null, null);
                 break;
 
 
@@ -44,5 +62,11 @@ public class MainActivity extends AppCompatActivity {
                 break;
 
         }
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        textToSpeech.shutdown();
     }
 }
