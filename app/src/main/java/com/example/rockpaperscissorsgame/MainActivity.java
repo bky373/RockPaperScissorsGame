c9cd48d3fdd58fc0d203ccb04320a0c1721a4536
bky373 <bo373@naver.com>
2019-12-03 12:55:30 +0900

    complete #4-1

--- app/src/main/java/com/example/rockpaperscissorsgame/MainActivity.java
+++ app/src/main/java/com/example/rockpaperscissorsgame/MainActivity.java
@@ -2,13 +2,27 @@ package com.example.rockpaperscissorsgame;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.drawable.AnimationDrawable;
+import android.media.Image;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    ImageView handAnimImageView;
+    AnimationDrawable animationDrawable;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        handAnimImageView = findViewById(R.id.hand_anim_image_view);
+        animationDrawable = (AnimationDrawable) handAnimImageView.getDrawable();
+        animationDrawable.start();
+
+    }
+
+    public void button_click(View view) {
     }
 }
