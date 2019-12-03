5372f3fab2ada892fe7b5cd538c7c24ef7d02db2
bky373 <bo373@naver.com>
2019-12-03 13:11:16 +0900

    complete #4-2

--- app/src/main/java/com/example/rockpaperscissorsgame/MainActivity.java
+++ app/src/main/java/com/example/rockpaperscissorsgame/MainActivity.java
@@ -11,6 +11,7 @@ import android.widget.ImageView;
 public class MainActivity extends AppCompatActivity {
 
     ImageView handAnimImageView;
+    ImageView setHandImageView;
     AnimationDrawable animationDrawable;
 
     @Override
@@ -18,11 +19,30 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         handAnimImageView = findViewById(R.id.hand_anim_image_view);
+        setHandImageView = findViewById(R.id.set_hand_image_view);
+        handAnimImageView.setVisibility(View.GONE);
+        setHandImageView.setVisibility(View.VISIBLE);
+
         animationDrawable = (AnimationDrawable) handAnimImageView.getDrawable();
-        animationDrawable.start();
 
     }
 
     public void button_click(View view) {
+        switch (view.getId()){
+            case R.id.replay_button:
+                setHandImageView.setVisibility(View.GONE);
+                handAnimImageView.setVisibility(view.VISIBLE);
+                animationDrawable.start();
+                break;
+
+
+            default:
+                animationDrawable.stop();
+                handAnimImageView.setVisibility(View.GONE);
+                setHandImageView.setVisibility(View.VISIBLE);
+                setHandImageView.setImageResource(R.drawable.com_scissors);
+                break;
+
+        }
     }
 }
